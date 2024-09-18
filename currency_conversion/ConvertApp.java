897f9193403102cea922a319332b47feb4415a13
ThanhHuy <167808835+ThanhHuy1908@users.noreply.github.com>
2024-09-18 18:22:28 -0500

    create currency conversion app

---
+++ currency_conversion/ConvertApp.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class ConvertApp {
+
+public static void main(String[] args) {
+    ConvertFrame convertFrame = new ConvertFrame();
+    convertFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    convertFrame.setVisible(true);
+
+}
+}
